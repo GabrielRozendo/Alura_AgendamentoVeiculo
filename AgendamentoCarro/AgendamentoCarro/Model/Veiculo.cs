df0c2ed1c7782fb0cb3727777508fca78ef1fd78
Gabriel Oliveira Rozendo <gabriel.rozendo@gmail.com>
2018-01-15 22:43:44 -0200

    Aula 2 finalizada. Itens exibidos e clicáveis...

---
+++ AgendamentoCarro/AgendamentoCarro/Model/Veiculo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace AgendamentoCarro
+{
+    public class Veiculo
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public string PrecoFormatado => string.Format($"R$ {Preco}");
+    }
+}
---
+++ AgendamentoCarro/AgendamentoCarro/Views/AgendamentoCarroPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace AgendamentoCarro
+{
+    public partial class AgendamentoCarroPage : ContentPage
+    {
+        public List<Veiculo> Veiculos { get; set; }
+
+
+        public AgendamentoCarroPage()
+        {
+
+            InitializeComponent();
+
+            Veiculos = new List<Veiculo>
+            {
+                new Veiculo { Nome = "Azera V6", Preco = 60000 },
+                new Veiculo { Nome = "Fiesta 2.0", Preco = 50000 },
+                new Veiculo { Nome = "HB20 S", Preco = 40000}
+            };
+
+            listViewVeiculos.ItemsSource = Veiculos;
+
+            this.BindingContext = this;
+        }
+
+        void Handle_ItemTapped(object sender, Xamarin.Forms.ItemTappedEventArgs e)
+        {
+            var veiculo = (Veiculo)e.Item;
+            DisplayAlert("Alura", $"O item {veiculo.Nome} foi tocado!{Environment.NewLine}{veiculo.PrecoFormatado}", "Blz");
+        }
+    }
+}
